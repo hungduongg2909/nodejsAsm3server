2a8b4156a5a6da04605a27497d5386600f588977
HungDuong2909 <hungduongg2909@gmail.com>
2025-07-10 15:14:26 +0700

    change init socket

--- socket.js
+++ socket.js
@@ -1,19 +1,22 @@
+const { Server } = require("socket.io"); 
+
 let io;
 
 module.exports = {
    init: (httpServer) => {
-      io = require("socket.io")(httpServer, {
+      io = new Server(httpServer, {
          cors: {
             origin: [
                "http://localhost:3000",
                "http://localhost:3001",
                "http://54.254.177.24:3000",
                "http://54.254.177.24:3001",
-                "https://famous-hotteok-6fb3ad.netlify.app",
+               "https://famous-hotteok-6fb3ad.netlify.app",
             ],
             methods: ["GET", "POST", "PUT", "DELETE"],
             credentials: true,
          },
+         transports: ["websocket", "polling"], 
       });
       return io;
    },
