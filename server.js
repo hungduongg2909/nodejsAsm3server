43f27f73995cd8bd710670c4f255e953d17f905f
HungDuong2909 <hungduongg2909@gmail.com>
2025-07-10 10:33:59 +0700

    change domain socket cors

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(
       "http://localhost:3001",
       "http://54.254.177.24:3000",
       "http://54.254.177.24:3001",
+      "https://famous-hotteok-6fb3ad.netlify.app",
    ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
--- socket.js
+++ socket.js
@@ -9,6 +9,7 @@ module.exports = {
                "http://localhost:3001",
                "http://54.254.177.24:3000",
                "http://54.254.177.24:3001",
+                "https://famous-hotteok-6fb3ad.netlify.app",
             ],
             methods: ["GET", "POST", "PUT", "DELETE"],
             credentials: true,
