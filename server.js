525321e27099153e55ef68865c1d0fa83f771ed4
HungDuong2909 <hungduongg2909@gmail.com>
2025-07-11 14:44:54 +0700

    update cors config

--- server.js
+++ server.js
@@ -24,17 +24,22 @@ const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_P
 
 // Middleware
 app.use(
-   cors()
+   cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+      allowedHeaders: ["Content-Type"],
+   })
    // cors({
-      // origin: [
-      // "http://localhost:3000",
-      // "http://localhost:3001",
-      // "http://54.254.177.24:3000",
-      // "http://54.254.177.24:3001",
-      // "https://famous-hotteok-6fb3ad.netlify.app",
-      // "https://686f7c25e3957f0008ed14db--famous-hotteok-6fb3ad.netlify.app",
-      // "https://soft-praline-a96359.netlify.app",
-      // "https://687083d34771cc5a8a51a00b--soft-praline-a96359.netlify.app",
+   // origin: [
+   // "http://localhost:3000",
+   // "http://localhost:3001",
+   // "http://54.254.177.24:3000",
+   // "http://54.254.177.24:3001",
+   // "https://famous-hotteok-6fb3ad.netlify.app",
+   // "https://686f7c25e3957f0008ed14db--famous-hotteok-6fb3ad.netlify.app",
+   // "https://soft-praline-a96359.netlify.app",
+   // "https://687083d34771cc5a8a51a00b--soft-praline-a96359.netlify.app",
    // ],
    //    methods: ["GET", "POST", "PUT", "DELETE"],
    //    credentials: true,
